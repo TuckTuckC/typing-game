15f3f311a3c2ee529040bb7408c7362ca8c7f27d
Tucker Craig <tuckermcraig@gmail.com>
2021-09-17 00:08:23 -0400

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Words from './components/Words';
 import './App.css';
 
 const App = () => {
+
+  const [word, setWord] = useState(Words);
+  const [newWord, setNewWord] = useState(word[0]);
+  const [disabled, setDisabled] = useState(true);
+  const [currentResults, setCurrentResults] = useState([]);
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCorrect, setCountCorrect] = useState(0);
+  const [time, setTime] = useState(30);
+  const [inputValue, setInputValue] = useState('');
+  const [animation, setAnimation] = useState(null);
+
   return (
     <div className="app">
       <div className="h1">Welcome</div>
